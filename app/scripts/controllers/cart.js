655b1981b94a56135483412bd8bbb9d53741e570
lshapz <laurashapiro01@gmail.com>
2017-05-14 15:48:50 -0400

    everything renders just need to configure ngCart directives to direct to spree and also play with css some more

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -8,8 +8,11 @@
  * Controller of the app
  */
 angular.module('sampleApp')
-.controller('ShopCartCtrl', ['$scope', 'ngCart', '$http', function ($scope, ngCart, $http) {
-
+.controller('ProductCheckoutCtrl', ['$scope', 'ngCart', '$http', function ($scope, ngCart, $http) {
+    var json = $http.post('https://nameless-sands-65262.herokuapp.com/api/v1/orders.json?token=8779437c5723b3cd5586bd590b4f9c1e02c448a02c3db7a7').then(function (response) {
+      let number = response.data.number
+    return number
+    });
    ngCart.setTaxRate(7.5);
    ngCart.setShipping(2.99);
 
--- app/scripts/services/addToCart.service.js
+++ app/scripts/services/addToCart.service.js
@@ -1,34 +1,37 @@
-// 'use strict';
+'use strict';
 
-// angular.module('sampleApp')
-//   .factory('Cart', function ($rootScope, $http) {
-//       var json = $http.post('https://nameless-sands-65262.herokuapp.com/api/v1/orders.json?token=8779437c5723b3cd5586bd590b4f9c1e02c448a02c3db7a7').then(function (response) {
-//       let number = response.data.number
-//     return number
-//     });
+angular.module('sampleApp')
+  .factory('Checkout', function ($rootScope, $http, cart) {
+      var json = $http.post('https://nameless-sands-65262.herokuapp.com/api/v1/orders.json?token=8779437c5723b3cd5586bd590b4f9c1e02c448a02c3db7a7').then(function (response) {
+      let number = response.data.number
+      
 
 
-//     return {
-//       query: function() {
-//         return json;
-//       },
-//       add: function(id) {
-//         var json = $http({
-//           url: "https://nameless-sands-65262.herokuapp.com/api/v1/orders/" + number + "/line_items?line_item[variant_id]=" + id + "&line_item[quantity]=1.json?token=8779437c5723b3cd5586bd590b4f9c1e02c448a02c3db7a7",
-//           method: 'POST',
-//           'X-Spree-Token': "8779437c5723b3cd5586bd590b4f9c1e02c448a02c3db7a7"
+    return number
+    });
 
-//       }).then((response=>{
+
+    return {
+      query: function() {
+        return json;
+      },
+      add: function(id) {
+        var json = $http({
+          url: "https://nameless-sands-65262.herokuapp.com/api/v1/orders/" + number + "/line_items?line_item[variant_id]=" + id + "&line_item[quantity]=1.json?token=8779437c5723b3cd5586bd590b4f9c1e02c448a02c3db7a7",
+          method: 'POST',
+          'X-Spree-Token': "8779437c5723b3cd5586bd590b4f9c1e02c448a02c3db7a7"
+
+      }).then((response=>{
         
-//         let item = response.data
+        let item = response.data
         
-//         return {
-//           price: item.display_amount,
-//           tax: item.total,       
-//         }
-//         }))
+        return {
+          price: item.display_amount,
+          tax: item.total,       
+        }
+        }))
 
-//         return json;
-//       }
-//     };
-//   });
+        return json;
+      }
+    };
+  });
